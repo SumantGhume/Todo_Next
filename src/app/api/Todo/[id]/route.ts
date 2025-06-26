7f7aece55ac1fa4c9b2923cddac8b658a556f9b4
suman <suman.sumantghume12@gmail.com>
2025-06-26 18:48:29 +0530

    Todo Web Application

--- src/app/api/Todo/[id]/route.ts
+++ src/app/api/Todo/[id]/route.ts
@@ -2,13 +2,16 @@ import connectDB from "@/lib/database";
 import Todo from "@/models/Todo";
 import { NextResponse } from "next/server";
 
-export async function GET({ params }:any) {
-    const {id} = await params
-    console.log(id)
-    await connectDB()
-    const todo = await Todo.findById(id)
-    console.log(todo)
-    return NextResponse.json({
-        todo: todo
-    })
+export async function GET(
+  _request: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = await params;
+  console.log(id);
+  await connectDB();
+  const todo = await Todo.findById(id);
+  console.log(todo);
+  return NextResponse.json({
+    todo: todo,
+  });
 }
