39681d8ddb656e03b96a7ac59a8d8a10de4b5a14
suman <suman.sumantghume12@gmail.com>
2025-06-26 18:40:03 +0530

    Todo Web Application

--- src/app/api/Todo/[id]/route.ts
+++ src/app/api/Todo/[id]/route.ts
@@ -1,22 +1,14 @@
 import connectDB from "@/lib/database";
-import ToDo from "@/models/Todo";
+import Todo from "@/models/Todo";
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
 
-// Use the correct context type
-export async function GET(
-  req: NextRequest,
-  { params }: { params: any }
-) {
-  const { id } = params;
-  console.log("Here we get the ID:", id);
-
-  await connectDB();
-  const todo = await ToDo.findById(id);
-
-  if (!todo) {
-    return NextResponse.json({ error: "Todo not found" }, { status: 404 });
-  }
-
-  return NextResponse.json({ todo });
+export async function GET({ params }:any) {
+    const {id} = await params
+    console.log(id)
+    await connectDB()
+    const todo = await Todo.findById(id)
+    console.log(todo)
+    return NextResponse.json({
+        todo: todo
+    })
 }
